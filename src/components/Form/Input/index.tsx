87b47240697d57398f7d2ed02128b3c021bbe01f
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-12 01:21:58 -0300

    Create InputText

---
+++ src/components/Form/Input/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import theme from 'styled-theming';
+
+interface IInputText {
+  width?: string;
+  label?: string;
+  type?: string;
+  value: string;
+  onChange: any;
+}
+
+const containerStyles = theme('mode', {
+  light: css`
+    background-color: ${(props) => props.theme.input.backgroundColor};
+    border-bottom: 2px solid ${(props) => props.theme.input.borderColor};
+    border-radius: 0 0 2px 2px;
+    padding: 8px 8px 24px 8px;
+    display: flex;
+    flex-direction: column;
+    color: ${(props) => props.theme.colors.primary};
+  `,
+});
+
+
+const Container = styled.div<{ inputWidth?: string }>`
+  width: ${(props) => (props.inputWidth)};
+  ${containerStyles}
+`;
+
+const labelStyles = theme('mode', {
+  light: css`
+    font-size: 14px;
+    font-weight: 500;
+    margin-bottom: 4px;   
+  `,
+});
+
+const Label = styled.label`
+  ${labelStyles}
+`;
+
+const inputStyles = theme('mode', {
+  light: css`
+    color: black;
+    font-weight: 600;
+    font-size: 18px;
+    background-color: transparent;
+    border: none;
+    &:focus {
+      border: none;
+      outline: none;
+    }
+  `,
+});
+
+const StyledInputText = styled.input`
+  ${inputStyles}
+`;
+
+
+const InputText: React.FC<IInputText> = ({
+  value, onChange, width, label, type = 'text',
+}) => (
+  <Container inputWidth={width}>
+    <Label>{label}</Label>
+    <StyledInputText onChange={onChange} value={value} type={type} />
+  </Container>
+);
+
+export default InputText;
