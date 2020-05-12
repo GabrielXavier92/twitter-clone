20e651e84060780f2384dc2004fc992ded2bd88b
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-12 01:22:16 -0300

    add variables to light theme

--- src/theme/light.ts
+++ src/theme/light.ts
@@ -1,3 +1,12 @@
-const light = {};
+const light = {
+  input: {
+    backgroundColor: '#f5f8fa',
+    borderColor: '#657786',
+    labelColor: '#657786',
+  },
+  card: {
+    borderColor: '#E1E8EE',
+  },
+};
 
 export default light;
