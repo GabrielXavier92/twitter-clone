741e7ed0e005b45b97a9b8d2d6986623eb446f29
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-16 15:02:31 -0300

    create HomePage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Home: React.FC = () => {
-  const home = 'home';
+import Container from '../../components/Container';
+import FeedCard from '../../components/FeedCard';
+import ProfileCard from '../../components/ProfileCard';
 
-  return (
-    <div>
-      Home
-      {home}
-    </div>
-  );
-};
+const Content = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: ${(props) => props.theme.spacers.spacer6} auto auto;
+`;
+
+const CoverPicture = styled.div`
+  position: absolute;
+  height: 200px;
+  width: 100%;
+  top: 0;
+  left: 0;
+  background-color: grey;
+`;
+
+const Home: React.FC = () => (
+  <Container>
+    <CoverPicture>
+      imagem principal
+    </CoverPicture>
+    <Content>
+      <ProfileCard
+        profileName="Gabriel Xavier"
+        profileTwitter="meuTwitter"
+        profileDescription="alguma descricao legal hahaha"
+        profileLocale="Brasil"
+        profilesStats={{ tweets: 30, followers: 50, following: 12 }}
+      />
+      <FeedCard width="40%" />
+    </Content>
+
+  </Container>
+);
 
 export default Home;
