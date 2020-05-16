638bf9279b6ad29a19ec203a63478c0a3b8a5609
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-16 15:01:02 -0300

    create Avatar component

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import defaultProfile from '../../assets/defaultProfile.png';
+
+interface IAvatar {
+  size?: string | number;
+  rounded?: boolean;
+  src?: string;
+  alt: string;
+}
+
+const StyleAvatar = styled.img<IAvatar>`
+  height: ${(props) => props.height};
+  width: ${(props) => props.width};
+  border-radius: ${(props) => (props.rounded ? '50%' : 0)};
+  max-height: 100%;
+  max-width: 100%;
+  margin-bottom: ${(props) => props.theme.spacers.spacer3};
+`;
+
+const Avatar: React.FC<IAvatar> = ({
+  alt, size, rounded, src,
+}) => (<StyleAvatar alt={alt} height={size} width={size} rounded={rounded} src={src || defaultProfile} />);
+
+export default Avatar;
