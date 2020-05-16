35bc0c141ec0122b20a1cab37cedcdc057379364
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-16 15:02:00 -0300

    create dark theme

---
+++ src/theme/dark.ts
@@ -0,0 +1,19 @@
+const dark = {
+  container: {
+    backgroudColor: '#FFF',
+  },
+  input: {
+    backgroundColor: '#f5f8fa',
+    borderColor: '#657786',
+    labelColor: '#657786',
+  },
+  card: {
+    borderColor: '#E1E8EE',
+    backgroudColor: '#FFF',
+  },
+  profileStats: {
+    dividerColor: 'red',
+  },
+};
+
+export default dark;
