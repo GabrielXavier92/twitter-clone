981b7d10a2cfc77e52c4329f5d9c9c98590348c4
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-15 22:40:41 -0300

    Create container component

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,27 +3,22 @@ import styled from 'styled-components';
 
 import Button from '../../components/Button';
 import Card from '../../components/Card';
+import Container from '../../components/Container';
 import Input from '../../components/Form/Input';
 
 import SignInImageLogo from '../../assets/signIn.png';
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 100px;
-  align-items: stretch;
-`;
 
 const CardBody = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  min-width: 300px;
 `;
 
 const SignInImage = styled.img`
-  height: 250px;
+  max-height: 100%;
+  max-width: 100%;
   margin-bottom: 16px;
 `;
 
@@ -44,20 +39,16 @@ const OrText = styled.span`
 
 const SignIn: React.FC = () => {
   const [name, setName] = useState('');
-
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setName(e.target.value);
-  };
+  const [password, setPassword] = useState('');
 
 
   return (
     <Container>
-      <Card>
+      <Card width="300px" height="600px" margin="100px 0">
         <CardBody>
           <SignInImage src={SignInImageLogo} alt="SignIn Image" />
-          <Input width="100%" label="Email" type="text" value={name} onChange={handleChange} />
-          <Input width="100%" label="Senha" type="password" value={name} onChange={handleChange} />
+          <Input width="100%" label="Email" type="text" value={name} onChange={(e) => { setName(e.target.value); }} />
+          <Input width="100%" label="Senha" type="password" value={password} onChange={(e) => { setPassword(e.target.value); }} />
           <ForgotText>Esqueceu sua senha?</ForgotText>
           <Button outlined width="100%"><ButtonText>Entrar</ButtonText></Button>
           <OrText>ou</OrText>
