528fbfe9773c556f1723c90c9c83fe4501fce7d3
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-15 23:19:01 -0300

    add spaces to default theme

--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -4,6 +4,20 @@ const defaultTheme = {
     hoverPrimary: '#1a91da',
     gray: '#e1e8ee',
   },
+  spacers: {
+    spacer0: '2px',
+    spacer1: '4px',
+    spacer2: '8px',
+    spacer3: '16px',
+    spacer4: '32px',
+    spacer5: '64px',
+    spacer6: '128px',
+  },
+  fonts: {
+    link: '12px',
+    label: '14px',
+    input: '18px',
+  },
 };
 
 export default defaultTheme;
