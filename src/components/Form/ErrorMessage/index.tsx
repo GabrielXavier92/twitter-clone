07b00442b70bdc810eb1ea1fa92d7b1b35448a11
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-16 03:10:46 -0300

    create errorMessage component

---
+++ src/components/Form/ErrorMessage/index.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const ErrorMessage = styled.span`
+  color: red;
+  font-size: ${(props) => props.theme.fonts.error};
+  margin-bottom: ${(props) => props.theme.spacers.spacer3}
+`;
+
+export default ErrorMessage;
