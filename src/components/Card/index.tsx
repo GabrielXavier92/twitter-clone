3e8b67f3430a825f9b55b6e405cde5c2932e3781
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-15 22:39:16 -0300

    Refactor card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
 import styled, { css } from 'styled-components';
 import theme from 'styled-theming';
 
+interface ICard {
+  width?: string;
+  height?: string;
+  margin?: string;
+}
+
 const cardStyles = theme('mode', {
+  light: css`
+  border-color: ${(props) => (props.theme.card.borderColor)};
+  border-width: 1px;
+  border-style: solid;
+  `,
   dark: css``,
 });
 
-const StyledCard = styled.div`
-  display: flex;
-  padding: 12px;
-  border-width: 1px;
-  border-style: solid;
-  border-color: ${(props) => (props.theme.card.borderColor)};
+const Card = styled.div<ICard>`
+  padding: 12px;  
   border-radius: 12px;
-  ${cardStyles};  
+  height: ${(props) => props.height};
+  width: ${(props) => props.width};
+  margin: ${(props) => props.margin};
+  ${cardStyles};
 `;
 
-const Card: React.FC = ({ children }) => (<StyledCard>{children}</StyledCard>);
 
 export default Card;
