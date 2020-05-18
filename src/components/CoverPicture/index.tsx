f121b7553082c3de8b8e9f6fe6e498eb88f7e5bf
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-17 21:52:27 -0300

    create cover picture component

---
+++ src/components/CoverPicture/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface IConverPicture {
+  src?: string;
+}
+
+const Content = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  height: 193px;
+  width: 100%;
+  top: 0;
+  left: 0;
+  background-color: grey;
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  height: 100%;
+`;
+
+const CoverPicture: React.FC<IConverPicture> = ({ src }) => (<Content><Image src={src} /></Content>);
+
+export default CoverPicture;
