47b938318ac71dac40c5b34787c7c6b31a82c935
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-17 21:53:31 -0300

    add styles and signOut in profileCard

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -9,10 +9,13 @@ import { TiEdit } from 'react-icons/ti';
 import Avatar from '../Avatar';
 import ProfileStats, { IProfileStats } from '../ProfileStats';
 
+import AuthenticationState from '../../context/authentication/state';
+
 import { Card, CardBody } from '../Card';
 
 interface IProfileCard {
   profileName?: string;
+  profilePhoto?: string;
   profileTwitter?: string;
   profileLocale?: string;
   profileDescription?: string;
@@ -20,8 +23,8 @@ interface IProfileCard {
 }
 
 const ProfileCardStyled = styled(Card)`
-  min-width: 150px;
-  max-width: 300px;
+  min-width: 250px;
+  max-width: 400px;
   padding-bottom: 0;
   margin-bottom: auto;
 `;
@@ -33,13 +36,19 @@ const ProfileName = styled.span`
 `;
 
 const EditAccount = styled.span`
+  position: absolute;
+  top: 0;
+  right: 0;
   font-size: ${(props) => props.theme.fonts.profile}; 
   color: ${(props) => props.theme.fonts.greyColor};
-  margin-bottom: ${(props) => props.theme.spacers.spacer3};
-  margin-left: auto;
   :hover {
     cursor: pointer;
   }
+`;
+
+const SigOut = styled(EditAccount)`
+  font-size: ${(props) => props.theme.fonts.link}; 
+  left: 0;
 `;
 
 const TwitterName = styled.span`
@@ -58,19 +67,25 @@ const Information = styled.span`
 const Description = Information;
 
 const ProfileCard: React.FC<IProfileCard> = ({
-  profileName, profileTwitter, profileLocale, profileDescription, profilesStats,
+  profileName, profilePhoto, profileTwitter, profileLocale, profileDescription, profilesStats,
 }) => {
   const history = useHistory();
+  const { handleSignOut } = useContext(AuthenticationState);
 
   const changePage = () => {
     history.push('/profile');
   };
 
+  const signOut = () => {
+    handleSignOut();
+  };
+
   return (
     <ProfileCardStyled>
       <CardBody>
+        <SigOut onClick={signOut}>Sair da conta</SigOut>
         <EditAccount onClick={changePage}><TiEdit /></EditAccount>
-        <Avatar rounded size={80} alt="Profile avatar" />
+        <Avatar rounded size={100} src={profilePhoto} alt="Profile avatar" />
         <ProfileName>{profileName}</ProfileName>
         <TwitterName>
           @
