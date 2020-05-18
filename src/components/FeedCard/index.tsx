e7036ec71b02bc7cc346985b20f49afcd6b53e4d
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-18 00:26:23 -0300

    add logic to filer twets

--- src/components/FeedCard/index.tsx
+++ src/components/FeedCard/index.tsx
@@ -1,12 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
 import { Card } from '../Card';
 
-import TweetTab, { ITab } from '../TweetTab';
+import TweetTab from '../TweetTab';
 import NewTweet from '../NewTweet';
 import TweetComponent from '../TweetComponent';
 
 import PostsState from '../../context/post/state';
+import UserState from '../../context/user/state';
 
 interface IFeedCard {
   minWidth?: string;
@@ -19,16 +20,42 @@ const FeedCardStyled = styled(Card)<IFeedCard>`
 
 const FeedCard: React.FC<IFeedCard> = ({ minWidth }) => {
   const { posts } = useContext(PostsState);
-  const tabs: Array<ITab> = [
+  const { user } = useContext(UserState);
+
+  const [tabs, setTabs] = useState([
     { id: '1', text: 'Tweets', active: true },
     { id: '2', text: 'Meus Tweets', active: false },
-  ];
+  ]);
+
+  const handleChangeTable = (id) => {
+    const newTabs = tabs.map((tab) => {
+      const newTab = tab;
+      if (tab.id === id) {
+        if (!tab.active) {
+          newTab.active = true;
+        }
+      } else {
+        newTab.active = false;
+      }
+      return newTab;
+    });
+
+    setTabs(newTabs);
+  };
+
+  const queryTweets = () => {
+    const activeTab = tabs.find((tab) => tab.active);
+    if (activeTab!.id === '2') {
+      return posts.filter((post) => (post.ownerId === user?.id)).map((post) => <TweetComponent key={post.timestamp} {...post} />);
+    }
+    return posts.map((post) => <TweetComponent key={post.timestamp} {...post} />);
+  };
 
   return ((
     <FeedCardStyled minWidth={minWidth}>
-      <TweetTab tabs={tabs} />
+      <TweetTab tabs={tabs} onClick={handleChangeTable} />
       <NewTweet />
-      {posts.map((post) => <TweetComponent key={post.timestamp} {...post} />)}
+      {queryTweets()}
     </FeedCardStyled>
   ));
 };
