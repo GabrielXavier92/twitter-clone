5f4d312b4187f1bc00d76d29ed1337af9a689fe1
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-18 00:28:34 -0300

    add order to posts

--- src/service/posts/index.ts
+++ src/service/posts/index.ts
@@ -1,10 +1,10 @@
-import { IPost } from 'src/interfaces/post';
+import { IPost } from '../../interfaces/post';
 import Firebase from '../../database/firebase';
 
 class UserService {
   createPost = (post: IPost) => Firebase.createPost(post);
 
-  getPosts = () => Firebase.getPosts();
+  getPosts = () => Firebase.getPosts().orderBy('timestamp', 'desc');
 }
 
 export default new UserService();
