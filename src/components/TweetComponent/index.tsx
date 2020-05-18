e4e3c88f26a523a320d9b19fec8a744bea493889
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-17 21:53:46 -0300

    reder real data in TweetComponent

--- src/components/TweetComponent/index.tsx
+++ src/components/TweetComponent/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import moment from 'moment';
 
 import { AiOutlineClockCircle } from 'react-icons/ai';
 
+import { IPost } from 'src/interfaces/post';
 import Avatar from '../Avatar';
 import { Card } from '../Card';
 
@@ -31,29 +33,35 @@ const ProfileName = styled.span`
 `;
 
 const TwitterName = styled.span`
-  font-size: ${(props) => props.theme.fonts.label}; 
+  font-size: ${(props) => props.theme.fonts.link}; 
   color: ${(props) => props.theme.fonts.greyColor};
+  margin-right: ${(props) => props.theme.spacers.spacer3};
 `;
 
 const Information = styled.span`
-  font-size: ${(props) => props.theme.fonts.description};
+  font-size: ${(props) => props.theme.fonts.label};
   margin-left: auto;
 `;
 
-const TweetComponent: React.FC = () => (
+const TweetComponent: React.FC<IPost> = ({
+ ownerName, ownerPhotoURL, timestamp, content 
+}) => (
   <Content>
-    <Avatar size={40} rounded alt="Profile avatar in new tweet" />
+    <Avatar size={40} src={ownerPhotoURL} rounded alt="Profile avatar in new tweet" />
     <TweetCard>
       <TweetProfile>
-        <ProfileName>Gabriel Xavier</ProfileName>
-        <TwitterName>@tweet</TwitterName>
+        <ProfileName>{ownerName}</ProfileName>
+        <TwitterName>
+          @
+          {ownerName}
+        </TwitterName>
         <Information>
           <AiOutlineClockCircle />
-          35min
+          {moment(timestamp).fromNow()}
         </Information>
       </TweetProfile>
       <div>
-        conteudo do tweet
+        {content}
       </div>
     </TweetCard>
   </Content>
